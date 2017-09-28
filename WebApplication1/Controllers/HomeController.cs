778aac76f49fefc576b1cd0643f464fad6fa7d2f
Oleksandr <voloshyn244@gmail.com>
2017-09-28 21:32:01 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.Entity;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        SoccerContext db = new SoccerContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var players = db.Players.Include(p => p.Team);
+            return View(players.ToList());
+        }
+    }
+}
